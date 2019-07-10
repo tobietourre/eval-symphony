77847f7b491f10742037a7736fdd7bf4784d4d33
tobie.tourre <tobie.tourre@ipilyon.net>
2019-07-10 15:30:43 +0200

    création CRUD pour les 3 entités

---
+++ src/Form/ProductType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('libelle')
+            ->add('description')
+            ->add('nb_likes')
+            ->add('prix')
+            ->add('category')
+            ->add('tag')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
